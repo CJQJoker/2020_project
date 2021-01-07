fd67e0d48d760fc0fce2fd6738ad315f837b5d8b
WLC-ui <76935907+WLC-ui@users.noreply.github.com>
2021-01-07 19:15:36 +0800

    添加从data_2.xls 中导出通知正文到 all-notice.txt 的代码

--- ciyun.py
+++ ciyun.py
@@ -1,3 +1,4 @@
+import xlrd
 from wordcloud import WordCloud
 import matplotlib.pyplot as plt
 import jieba
@@ -5,7 +6,17 @@ import numpy as np
 from PIL import Image
 
 
+#字典用于存储词频
 counts = {}
+#打开xls文件
+ad_wb = xlrd.open_workbook("data_2.xls")
+#获取第一个目标表单
+sheet_0 = ad_wb.sheet_by_index(0)
+#生成存有所有通知正文的txt
+for i in range(sheet_0.nrows):
+    with open("all-notice.txt","a", encoding="utf-8") as f:
+        f.write(sheet_0.cell_value(i,3))
+        f.write('\n')
 
 text = open("{}.txt".format('all-notice'), encoding="utf-8").read()
 jieba.load_userdict('dict.txt')
